14ca51f89352ead3c6813f535712f98361dca7c3
thancode99382 <dat99382@donga.edu.vn>
2023-12-01 15:34:28 +0700

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Body from './components/Body/Body'
 import Footer from './components/Footer'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header/>
+      <hr />
+      <Body/>
+      <hr />
       <Footer/>
     </div>
   )
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,14 @@
-import styles from "./Footer.module.scss";
+/* import styles from "./Footer.module.scss";
 import classNames from "classnames/bind";
 
 export default function Footer() {
   const cx = classNames.bind(styles); // Bind the styles object to classNames
   return <div className={cx("test")}>Footer</div>;
+}
+ */
+
+
+
+export default function Footer() {
+  return <h1>Footer</h1>
 }
