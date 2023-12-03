c2cfeb86aea81c957e681fcc1e3f08bec5c4b5aa
thancode99382 <dat99382@donga.edu.vn>
2023-12-03 10:03:29 +0700

    modify id isbn13

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,10 +2,11 @@ import BookShow from "./BookShow";
 import PropTypes from "prop-types";
 
 export default function BookList({ books }) {
+  
   return (
     <div className="book-list row mt-5">
-      {books.map((book, index) => (
-        <div className="col-md-4" key={index}>
+      {books.map((book) => (
+        <div className="col-md-4" key={book.isbn13}>
           <BookShow book={book} />
         </div>
       ))}
