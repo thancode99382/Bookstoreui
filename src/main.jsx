f20efe9b461aaee529e8a8bd53945a3e49624019
ducchinhpro123 <chinh99562@donga.edu.vn>
2023-12-03 13:01:34 +0700

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-<<<<<<< HEAD
 import { CartProvider } from "./components/CartContext.jsx";
-=======
 import newBooks from "./api/defaultNewBookApi.js";
 
->>>>>>> 7ea665b9fabbde2ad0c2f6cedc1b9cfd7de0bdab
 /* const router = createBrowserRouter([
   {
     path: '/',
