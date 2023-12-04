5f2dbe543935018330ca8b4323bbaad11badeb63
thancode99382 <dat99382@donga.edu.vn>
2023-12-04 14:05:30 +0700

    done

--- src/components/CardProductProvider.jsx
+++ src/components/CardProductProvider.jsx
@@ -1,12 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import propTypes from "prop-types";
 import { useState } from "react";
 const CardProductContext = createContext();
+
 function CardProductProvider({ children }) {
-  const [cardProduct, setCardProduct] = useState();
+  const [cardProduct, setCardProduct] = useState({});
+
+
+  useEffect(() => {
+    const storedProduct = localStorage.getItem('cardProduct')
+    if (storedProduct) {
+      setCardProduct(JSON.parse(storedProduct))
+    }
+  }, [])
+
   function setProduct(book) {
     setCardProduct(book);
+    localStorage.setItem("cardProduct", JSON.stringify(book))
   }
+  
   return (
     <CardProductContext.Provider value={{ cardProduct, setProduct }}>
       {children}
